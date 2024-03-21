784377b1d4ad6b78ea2fe87387c44e516e479f1a
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-03-21 13:56:57 +0000

    updating project name after deployment from template repository

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/health/HealthInfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/health/HealthInfoTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename.health
+package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.health
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
