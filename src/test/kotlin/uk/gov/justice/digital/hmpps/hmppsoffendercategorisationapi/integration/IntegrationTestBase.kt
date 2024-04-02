223aa29bf7986872492264d94e1ee399c664d76e
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-04-02 12:54:21 +0100

    si-809 adding necessary auth, openapi and health endpoints with minimal tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/integration/IntegrationTestBase.kt
@@ -4,16 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.microsoft.applicationinsights.TelemetryClient
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.http.HttpHeaders
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import uk.gov.justice.digital.hmpps.hmppshdcapi.integration.wiremock.OAuthMockServer
 import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.config.JwtAuthHelper
 
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
 abstract class IntegrationTestBase {
@@ -57,7 +62,7 @@ abstract class IntegrationTestBase {
   }
 
   protected fun setAuthorisation(
-    user: String = "hmpps-hdc-api-1",
+    user: String = "test-client",
     roles: List<String> = listOf(),
   ): (HttpHeaders) -> Unit = jwtAuthHelper.setAuthorisation(user, roles)
 
