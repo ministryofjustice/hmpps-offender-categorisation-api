370d11292fd4b52aa6521192b61b7a0b51a82dd3
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-04-23 14:50:48 +0100

    si-777 extract sar info from risk profiler

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/PreviousProfileEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/PreviousProfileEntity.kt
@@ -2,8 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.time.ZonedDateTime
