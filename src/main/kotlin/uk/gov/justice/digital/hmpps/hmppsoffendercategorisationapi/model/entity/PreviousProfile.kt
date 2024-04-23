dbe5c396d5b3529af29a50c3dd71c593d0062bb3
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-04-23 12:30:11 +0100

    si-777 extract sar info from risk profiler

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/PreviousProfile.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotNull
+import org.hibernate.validator.constraints.Length
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "PREVIOUS_PROFILE")
+data class PreviousProfile(
+  @Id
+  val offenderNo: @Length(max = 10) String,
+  var escape: String,
+  var soc: String,
+  var violence: String,
+  var executeDateTime: @NotNull LocalDateTime,
+)
--- src/test/resources/repository/reset.sql
+++ src/test/resources/repository/reset.sql
@@ -108,6 +108,23 @@ CREATE TABLE public.security_referral (
                                           CONSTRAINT security_referral_pkey PRIMARY KEY (id)
 );
 CREATE INDEX security_referral_prison_id_index ON public.security_referral USING btree (prison_id);
+
+DROP TABLE IF EXISTS PREVIOUS_PROFILE
+
+CREATE TABLE PREVIOUS_PROFILE
+(
+    OFFENDER_NO       VARCHAR(10) PRIMARY KEY,
+    ESCAPE            TEXT,
+    EXTREMISM         TEXT,
+    SOC               TEXT,
+    VIOLENCE          TEXT,
+    EXECUTE_DATE_TIME TIMESTAMP   NOT NULL,
+);
+
+COMMENT ON TABLE PREVIOUS_PROFILE IS 'Records the previous risk profile results for an offender';
+
+GRANT SELECT, INSERT, UPDATE ON PREVIOUS_PROFILE TO risk_profiler;
+
 --DELETE FROM risk_change;
 --DELETE FROM form;
 --DELETE FROM lite_category;
