51a96c6b3178c4f50778443a8009a2f4c00e7bb7
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-10-07 10:36:56 +0100

    si-1246 get latest risk profile change record (#12)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/repository/offendercategorisation/RiskChangeRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/repository/offendercategorisation/RiskChangeRepositoryTest.kt
@@ -14,9 +14,9 @@ class RiskChangeRepositoryTest : ResourceTest() {
   @Test
   @Sql("classpath:repository/risk_change.sql")
   @Sql(scripts = ["classpath:repository/reset.sql"], executionPhase = AFTER_TEST_METHOD)
-  fun `Should Find by Offender No`() {
-    val riskChange = repository.findByOffenderNo("G0048VL")
+  fun `Should Find First by Offender No Order By Raised Date`() {
+    val riskChange = repository.findFirstByOffenderNoOrderByRaisedDateDesc("G0048VL")
 
-    assertThat(riskChange?.userId).isEqualTo("LBENNETT_GEN")
+    assertThat(riskChange?.prisonId).isEqualTo("3")
   }
 }
