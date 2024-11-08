e88c67bbd48a2fc6d456e4a95b841a2dc2c2937f
Christopher Moss <christopher.moss@digital.justice.gov.uk>
2024-11-08 13:53:32 +0000

    [SI-1282] Ktlint fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/services/PrisonerOffenderSearchEventListenerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/services/PrisonerOffenderSearchEventListenerService.kt
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Service
 
 @Service
 class PrisonerOffenderSearchEventListenerService(
-    private val mapper: ObjectMapper,
+  private val mapper: ObjectMapper,
 ) {
-    private companion object {
-        val log: Logger = LoggerFactory.getLogger(this::class.java)
-    }
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
 
-    @SqsListener("prisoneroffendersearch", factory = "hmppsQueueContainerFactoryProxy")
-    fun processMessage(rawMessage: String) {
-        val (message, messageAttributes) = mapper.readValue(rawMessage, Message::class.java)
-        val eventType = messageAttributes.eventType.Value
-        log.info("Received message $message, type $eventType")
-    }
+  @SqsListener("prisoneroffendersearch", factory = "hmppsQueueContainerFactoryProxy")
+  fun processMessage(rawMessage: String) {
+    val (message, messageAttributes) = mapper.readValue(rawMessage, Message::class.java)
+    val eventType = messageAttributes.eventType.Value
+    log.info("Received message $message, type $eventType")
+  }
 }
 
 data class EventType(val Value: String, val Type: String)
 data class MessageAttributes(val eventType: EventType)
 data class Message(
-    val Message: String,
-    val MessageAttributes: MessageAttributes,
+  val Message: String,
+  val MessageAttributes: MessageAttributes,
 )
