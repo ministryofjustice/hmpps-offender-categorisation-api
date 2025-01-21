cf1ea1d83824a8e9d701079b19a645e6017e216d
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-01-21 07:12:40 +0000

    updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
   kotlin("plugin.spring") version "2.0.20"
   kotlin("plugin.jpa") version "2.0.20"
   kotlin("plugin.serialization") version "1.9.25"
@@ -34,7 +34,7 @@ dependencies {
   implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
   implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.5.1")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.1")
 
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
