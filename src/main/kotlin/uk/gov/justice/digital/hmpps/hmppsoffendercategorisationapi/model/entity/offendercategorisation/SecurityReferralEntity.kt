981edac36bb64a7b39d3418a2ec2e00b45871b98
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-02-03 13:39:44 +0000

    lint error

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/offendercategorisation/SecurityReferralEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/offendercategorisation/SecurityReferralEntity.kt
@@ -1,6 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.entity.offendercategorisation
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.annotations.JdbcType
 import org.hibernate.dialect.PostgreSQLEnumJdbcType
 import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.enum.SecurityReferralStatus
