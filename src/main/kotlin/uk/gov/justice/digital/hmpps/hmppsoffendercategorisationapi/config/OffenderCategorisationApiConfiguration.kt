6987172fdb45b3815847ccbe762c5263147612b5
timothy-ness <160499954+timothy-ness@users.noreply.github.com>
2025-04-09 07:28:31 +0100

    Adding in clock configuration (#49)
    * added in service function
    
    * fix for a couple of tests
    
    * added debugging
    
    * slight test change
    
    * slight test change
    
    * adding in docker test time and more tests
    
    * removed old clock configuration
    
    * added back in old clock configuration
    
    * removed logs
    
    * removed unused file
    
    * added configuration file
    
    * code review

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/config/OffenderCategorisationApiConfiguration.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import java.time.Clock
+
+@Configuration
+class OffenderCategorisationApiConfiguration {
+  @Bean
+  @Profile("!test")
+  fun clock(): Clock = Clock.systemUTC()
+}
