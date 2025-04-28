146483ec869b22b39cd977a03fa0a8ffe26c0299
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-04-28 08:44:05 +0100

    checking the status of a prisoner before cancel release

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/client/PrisonerSearchApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/client/PrisonerSearchApiClient.kt
@@ -1,18 +1,42 @@
 package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.client
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.client.ClientUtility.Companion.isNotFoundError
+import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.response.Prisoner
+import java.time.Duration
 
 @Service
 class PrisonerSearchApiClient(
   @Qualifier("prisonerSearchApiWebClient") private val webClient: WebClient,
+  @Value("\${prisoner.search.timeout:10s}") private val apiTimeout: Duration,
 ) {
+  fun findPrisoner(prisonerNumber: String): Prisoner = findPrisonerByIdAsMono(prisonerNumber)
+    .onErrorResume { e ->
+      if (!isNotFoundError(e)) {
+        log.error("Failed to get prisoner with id - $prisonerNumber on prisoner search")
+        Mono.error(e)
+      } else {
+        log.error("Prisoner with id - $prisonerNumber not found.")
+        Mono.error { NotFoundException("Prisoner with id - $prisonerNumber not found on prisoner search") }
+      }
+    }
+    .blockOptional(apiTimeout).orElseThrow { NotFoundException("Prisoner with id - $prisonerNumber not found on prisoner search") }
+
+  private fun findPrisonerByIdAsMono(prisonerId: String): Mono<Prisoner> = webClient.get().uri("/prisoner/$prisonerId")
+    .retrieve()
+    .bodyToMono()
+
   fun findPrisonersByPrisonerNumbers(prisonerNumbers: List<String>): List<Prisoner> {
     val requestBody = HashMap<String, List<String>>()
     requestBody["prisonerNumbers"] = prisonerNumbers
@@ -33,6 +57,10 @@ class PrisonerSearchApiClient(
     .bodyToMono(object : ParameterizedTypeReference<SearchResult>() {})
     .block()!!
     .content
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 }
 
 data class SearchResult(
