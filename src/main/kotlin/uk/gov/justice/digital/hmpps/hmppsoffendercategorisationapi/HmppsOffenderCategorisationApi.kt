4e297086c0a352dd86c350aa6a7fb8fd17f1a18a
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-09-15 13:23:09 +0100

    config for scheduler and correct cron format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/HmppsOffenderCategorisationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/HmppsOffenderCategorisationApi.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 class HmppsOffenderCategorisationApi
@@ -9,3 +11,7 @@ class HmppsOffenderCategorisationApi
 fun main(args: Array<String>) {
   runApplication<HmppsOffenderCategorisationApi>(*args)
 }
+
+@Configuration
+@EnableScheduling
+class SchedulingConfiguration
