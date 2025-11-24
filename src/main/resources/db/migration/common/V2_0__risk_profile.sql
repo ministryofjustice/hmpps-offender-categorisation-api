0a82b4fc34f274b3a74869fbbf38a0f400b82f63
timothy-ness <160499954+timothy-ness@users.noreply.github.com>
2025-11-24 10:02:58 +0000

    Update src/main/resources/db/migration/common/V2_0__risk_profile.sql
    Co-authored-by: Evald <evalds.spedzevs@justice.gov.uk>

--- src/main/resources/db/migration/common/V2_0__risk_profile.sql
+++ src/main/resources/db/migration/common/V2_0__risk_profile.sql
@@ -2,5 +2,5 @@ create table if not exists prisoner_risk_profile
 (
     offender_no       varchar(255) not null primary key,
     risk_profile      jsonb,
-    calculated_at timestamp,
+    calculated_at timestamp with time zone
 );
