dc141deb24d652c2e0b72211679d79679f3505a1
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-04-27 07:46:25 +0100

    added annotation for json column

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/offendercategorisation/PrisonerRiskProfileEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoffendercategorisationapi/model/entity/offendercategorisation/PrisonerRiskProfileEntity.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.entity.offendercategorisation
 
+import io.hypersistence.utils.hibernate.type.json.JsonType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.IdClass
 import jakarta.persistence.Table
+import org.hibernate.annotations.Type
 import uk.gov.justice.digital.hmpps.hmppsoffendercategorisationapi.model.entity.riskprofiler.OffenderNo
 import java.time.ZonedDateTime
 
@@ -16,6 +18,7 @@ class PrisonerRiskProfileEntity(
   @Column(name = "offender_no")
   val offenderNo: String,
 
+  @Type(JsonType::class)
   @Column(columnDefinition = "jsonb", name = "risk_profile")
   val riskProfile: String? = null,
 
